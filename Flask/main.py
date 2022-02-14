acf5504e0153ad7ad47a65cd10acb4e85cc70078
omartipe <oriolmartinez314@gmail.com>
2022-02-14 17:51:07 +0100

    Third commit

--- Flask/main.py
+++ Flask/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, make_response
+
 
 app = Flask(__name__)
+app.secret_key = "1233"
 
 @app.route('/hello/<name>')
 def hello_world(name):
@@ -13,10 +15,6 @@ def show_blog(postID):
 @app.route('/rev/<float:revNo>/')
 def revision(revNo):
     return 'Revsion number %f' % revNo
-
-@app.route('/admin')
-def hello_admin():
-   return 'Hello Admin'
 
 @app.route('/guest/<guest>')
 def hello_guest(guest):
@@ -33,27 +31,48 @@ def hello_user(name):
 def success(name):
    return 'welcome %s' % name
 
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+# @app.route('/login',methods = ['POST', 'GET'])
+# def login():
+#    if request.method == 'POST':
+#       user = request.form['nm']
+#       return redirect(url_for('success',name = user))
+#    else:
+#       user = request.args.get('nm')
+#       return redirect(url_for('success',name = user))
 
 @app.route('/hello/<int:score>')
 def hello_name(score):
    return render_template('hello.html', marks = score)
 
-@app.route('/result')
+@app.route('/result',methods = ['POST', 'GET'])
 def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+   if request.method == 'POST':
+      result = request.form
+      return render_template('result.html', result = result)
 
-@app.route("/")
-def index():
-   return render_template("index.html")
+# @app.route("/")
+# def student():
+#    return render_template("student.html")
+
+# @app.route("/")
+# def index():
+#   return render_template('login.html')
+
+
+@app.route('/setcookie', methods=['POST', 'GET'])
+def setcookie():
+   if request.method == 'POST':
+      user = request.form['nm']
+
+   resp = make_response(render_template('readcookie.html'))
+   resp.set_cookie('userID', user)
+
+   return resp
+
+@app.route('/getcookie')
+def getcookie():
+   name = request.cookies.get('userID')
+   return '<h1>welcome '+name+'</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
