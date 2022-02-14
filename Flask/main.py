2f4b8898430e0237cd4b0718e2c817074f3256ee
omartipe <oriolmartinez314@gmail.com>
2022-02-14 16:49:44 +0100

    Second commit

--- Flask/main.py
+++ Flask/main.py
@@ -51,5 +51,9 @@ def result():
    dict = {'phy':50,'che':60,'maths':70}
    return render_template('result.html', result = dict)
 
+@app.route("/")
+def index():
+   return render_template("index.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
